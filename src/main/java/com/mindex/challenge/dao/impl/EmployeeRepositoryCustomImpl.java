7fe1b72bbbcdc9bb17a195192c9d717d5a8bcd5d
mindex <dev@mindex.com>
2025-02-20 21:28:34 -0500

    More Documentation

--- src/main/java/com/mindex/challenge/dao/impl/EmployeeRepositoryCustomImpl.java
+++ src/main/java/com/mindex/challenge/dao/impl/EmployeeRepositoryCustomImpl.java
@@ -5,12 +5,18 @@ import com.mindex.challenge.entities.Compensation;
 import com.mindex.challenge.entities.Employee;
 import java.util.Set;
 import java.util.UUID;
+
+import jakarta.annotation.Nonnull;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Custom repository for needs where MongoRepository implemented classes fall short.
+ * Working with mongo template allows for more flexibility in our queries.
+ */
 @Repository
 public class EmployeeRepositoryCustomImpl implements EmployeeRepositoryCustom {
     private final MongoTemplate mongoTemplate;
@@ -19,8 +25,14 @@ public class EmployeeRepositoryCustomImpl implements EmployeeRepositoryCustom {
         this.mongoTemplate = mongoTemplate;
     }
 
+    /**
+     * Adds a set of compensations to an employee in the database.
+     *
+     * @param employeeId Unique Identifier for employee collection
+     * @param compensations A set of {@link Compensation} objects to be added to the employee's compensations.
+     */
     @Override
-    public void addCompensationsToEmployee(UUID employeeId, Set<Compensation> compensations) {
+    public void addCompensationsToEmployee(@Nonnull UUID employeeId, @Nonnull Set<Compensation> compensations) {
         Query query = new Query(Criteria.where("employeeId").is(employeeId));
 
         Update update = new Update().push("compensations").each(compensations.toArray());
